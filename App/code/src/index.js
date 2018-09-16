101ee02c2123402bb6c8b88bd182fe2a0cb19aed
Whitney Wong <whitney.wong@utexas.edu>
2018-09-16 14:41:43 -0500

    Initial commit of working cordova project with react app.

---
+++ App/code/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+const startApp = () => {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
+}
+  
+if(!window.cordova) {
+  startApp()
+} else {
+  document.addEventListener('deviceready', startApp, false)
+}
