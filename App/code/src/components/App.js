4d65ad40005bf04c5b5cce92d8b9c1513f01d48f
Whitney Wong <whitney.wong@utexas.edu>
2018-10-27 12:25:48 -0500

    Moved App.js into components directory.

--- App/code/src/components/App.js
+++ App/code/src/components/App.js
@@ -3,8 +3,8 @@
 */
 
 import React, { Component } from 'react';
-import logo from './assets/logo.svg';
-import './css/App.css';
+import logo from '../assets/logo.svg';
+import '../css/App.css';
 
 class App extends Component {
   render() {
@@ -12,7 +12,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React, Whitney!!</h1>
+          <h1 className="App-title">Welcome to React, Whitney 😃</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
